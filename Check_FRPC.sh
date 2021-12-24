c10a1690a62f521d88454e00e2126f304da67093
Yang2635 <mail@isisy.com>
2021-12-24 16:08:21 +0800

    Add: battery detection, screen status detection. Module detection logic

--- Check_FRPC.sh
+++ Check_FRPC.sh
@@ -1,14 +1,21 @@
 #!/system/bin/sh
 
 MODDIR=${0%/*}
-. ${MODDIR}/files/status.conf
 
-DATADIR="/sdcard/Android"
-Busybox_file="${MODDIR}/files/bin/busybox_${F_ARCH}"
-
-PROCESS()
-{
+PROCESS(){
 	ps -ef | grep "frpc-${F_ARCH}" | grep -v grep | wc -l
+}
+
+Battery_Charge(){
+	dumpsys battery | grep powered | grep 'true' | wc -l
+}
+
+Battery_Electricity(){
+	dumpsys battery | awk '/level: /{print $2}'
+}
+
+Screen_status(){
+	dumpsys deviceidle | awk -F'=' '/mScreenOn/{print $2}' | sed -e 's/false/1/' -e 's/true/0/'
 }
 
 Runing_Start(){
@@ -18,17 +25,22 @@ Runing_Start(){
 		if [[ $? -eq 0 ]]; then
 			sed -i "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确！" "${MODDIR}/files/status.conf"
 			sh ${MODDIR}/Run_FRPC.sh start
-			sleep 1
+			sleep 5
 			if [[ $(PROCESS) -ne 0 ]]; then
-				sed -i -e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC正在运行中！" -e "/^RELOAD_NUM=/c RELOAD_NUM=0" "${MODDIR}/files/status.conf"
+				Running_NUM=$(sh ${MODDIR}/Run_FRPC.sh status)
+				sed -i -e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC正在运行中！" \
+				-e "/^RELOAD_NUM=/c RELOAD_NUM=0" -e "/^RUNNING_NUM=/c RUNNING_NUM=${Running_NUM}" \
+				"${MODDIR}/files/status.conf"
 			else
 				sed -i "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC启动失败！" "${MODDIR}/files/status.conf"
 			fi
 		else
-			sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测错误，请自查配置文件！" -e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC未运行！" "${MODDIR}/files/status.conf"
+			sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测错误，请自查配置文件！" \
+			-e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC未运行！" "${MODDIR}/files/status.conf"
 		fi
 	else
-		sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=Android\/frpc目录下未找到frpc\.ini文件！" -e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC未运行！" "${MODDIR}/files/status.conf"
+		sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=Android\/frpc目录下未找到frpc\.ini文件！" \
+		-e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC未运行！" "${MODDIR}/files/status.conf"
 	fi
 }
 
@@ -36,11 +48,16 @@ Check_Reload(){
 	if [[ -f ${DATADIR}/frpc/frpc.ini ]]; then
 		check_new_file_status=$(stat -c %Y ${DATADIR}/frpc/frpc.ini)
 		if [[ "${FILE_STATUS}" != "$check_new_file_status" ]]; then
-			sh ${MODDIR}/Run_FRPC.sh verify
+			sh ${MODDIR}/Run_FRPC.sh verify 
 			if [[ $? -eq 0 ]]; then
 				sh ${MODDIR}/Run_FRPC.sh reload
 				if [[ $? -eq 0 ]]; then
-					sed -i -e "/^RELOAD_NUM=/c RELOAD_NUM=$(($RELOAD_NUM+1))" -e "/^FILE_STATUS=/c FILE_STATUS=${check_new_file_status}" -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确！" "${MODDIR}/files/status.conf"
+					sleep 2
+					Running_NUM=$(sh ${MODDIR}/Run_FRPC.sh status)
+					sed -i -e "/^RELOAD_NUM=/c RELOAD_NUM=$(($RELOAD_NUM+1))" \ 
+					-e "/^FILE_STATUS=/c FILE_STATUS=${check_new_file_status}" \
+					-e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确！" \
+					-e "/^RUNNING_NUM=/c RUNNING_NUM=${Running_NUM}" "${MODDIR}/files/status.conf"
 				else
 					sed -i "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确但重载失败，请自查配置文件！（已保持当前运行状态）" "${MODDIR}/files/status.conf"
 				fi
@@ -54,7 +71,20 @@ Check_Reload(){
 }
 
 Main(){
-	if [[ ! -f ${MODDIR}/disable ]]; then
+	if [[ $(Battery_Electricity) -lt 20 ]] && [[ $(Battery_Charge) -eq 0 ]]; then
+		if [[ $(PROCESS) -ne 0 ]]; then
+			kill -9 $(ps -ef | grep "frpc-${F_ARCH}" | grep -v grep | awk '{ print $2 }')
+			if [[ $? -eq 0 ]]; then
+				sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=已自动停止检测！" \
+				-e "/^RUNNING_STATUS=/c RUNNING_STATUS=当前电量低于20%且未在充电，自动停止运行！" \
+				-e "/^RUNNING_NUM=/c RUNNING_NUM=已自动停止检测！" "${MODDIR}/files/status.conf"
+			fi
+		else
+			sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=已自动停止检测！" \
+			-e "/^RUNNING_STATUS=/c RUNNING_STATUS=当前电量低于20%且未在充电，自动停止运行！" \
+			-e "/^RUNNING_NUM=/c RUNNING_NUM=已自动停止检测！" "${MODDIR}/files/status.conf"
+		fi
+	elif [[ ! -f ${MODDIR}/disable ]]; then
 		if [[ $(PROCESS) -eq 0 ]]; then
 			Runing_Start
 		else
@@ -64,24 +94,36 @@ Main(){
 		if [[ $(PROCESS) -ne 0 ]]; then
 			kill -9 $(ps -ef | grep "frpc-${F_ARCH}" | grep -v grep | awk '{ print $2 }')
 			if [[ $? -eq 0 ]]; then
-				sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=已手动停止检测！" -e "/^RUNNING_STATUS=/c RUNNING_STATUS=已手动停止运行（重新打开则自动重启FRPC，无需设备重启）" "${MODDIR}/files/status.conf"
+				sed -i -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=已手动停止检测！" \
+				-e "/^RUNNING_STATUS=/c RUNNING_STATUS=已手动停止运行（重新打开则自动重启FRPC，无需设备重启）" \
+				-e "/^RUNNING_NUM=/c RUNNING_NUM=已手动停止检测！" "${MODDIR}/files/status.conf"
 			fi
 		fi
 	fi
 	. ${MODDIR}/files/status.conf
 	if [[ ! -f ${MODDIR}/update ]]; then
-		sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：$RUNNING_STATUS ]，[配置文件状态：$CHECK_FILE_STATUS；自动重载配置文件 $RELOAD_NUM 次]，[设备架构：$F_ARCH，检测方式：$RUNNING_METHOD]" "${MODDIR}/module.prop"
+		sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：$RUNNING_STATUS ]，[配置文件状态：$CHECK_FILE_STATUS；穿透服务数：$RUNNING_NUM，自动重载配置文件 $RELOAD_NUM 次]，[检测方式：$RUNNING_METHOD]" "${MODDIR}/module.prop"
 	else
-		sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：$RUNNING_STATUS ]，[配置文件状态：$CHECK_FILE_STATUS；自动重载配置文件 $RELOAD_NUM 次]，[设备架构：$F_ARCH，检测方式：$RUNNING_METHOD]（模块新设定将在设备重启后生效！）" "${MODDIR}/module.prop"
+		sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：$RUNNING_STATUS ]，[配置文件状态：$CHECK_FILE_STATUS；穿透服务数：$RUNNING_NUM，自动重载配置文件 $RELOAD_NUM 次]，[检测方式：$RUNNING_METHOD]（模块新设定将在设备重启后生效！）" "${MODDIR}/module.prop"
+	fi
+}
+
+Start(){
+	. ${MODDIR}/files/status.conf
+	Busybox_file="${MODDIR}/files/bin/busybox_${F_ARCH}"
+	if [[ ${SCREEN_STATUS} == 'yes' ]]; then
+		Main
+	elif [[ $(Screen_status) -eq 0 ]]; then
+		Main
 	fi
 }
 
 if [[ ! -f "${Busybox_file}" ]] && [[ -z "$(which crond)" ]]; then
 	while :
 	do
-		Main
+		Start
 		sleep 60
 	done
 else
-	Main
+	Start
 fi

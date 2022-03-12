bd06277d05f70012acca997b854343b930b3ef42
Yang2635 <mail@isisy.com>
2022-03-12 15:38:40 +0800

    Update version number

--- customize.sh
+++ customize.sh
@@ -1,67 +1,65 @@
 SKIPUNZIP=1
 
 DATADIR='/sdcard/Android'
-VERSION='v2.8'
-VERSIONCODE='20220130'
+VERSION='v2.9'
+VERSIONCODE='20220312'
 
 unzip -o "${ZIPFILE}" 'module.prop' -d "${TMPDIR}" >&2
 [[ ! -f "${TMPDIR}/module.prop" ]] && abort "! 未找到module.prop文件，安装结束！"
 
-Cus_Print() 
-{
+Cus_Print(){
 	ui_print "$@"
 	sleep 0.03
 }
 
 file="META-INF/com/google/android/update-binary"
-file_path="${TMPDIR}/$file"
-hash_path="$file_path.sha256sum"
-unzip -o "$ZIPFILE" "META-INF/com/google/android/*" -d "${TMPDIR}" >&2
-[ -f "$file_path" ] || abort "! $file 不存在！"
-if [ -f "$hash_path" ]; then
-  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort "！$file 校验失败！"
-  Cus_Print "- 校验：$file" >&1
+file_path="${TMPDIR}/${file}"
+hash_path="${file_path}.sha256sum"
+unzip -o "${ZIPFILE}" "META-INF/com/google/android/*" -d "${TMPDIR}" >&2
+[ -f "${file_path}" ] || abort "! ${file} 不存在！"
+if [ -f "${hash_path}" ]; then
+  (echo "$(cat "${hash_path}")  ${file_path}" | sha256sum -c -s -) || abort "！校验：${file} 错误！"
+  Cus_Print "- 校验：${file}" >&1
 else
   abort "！缺少校验文件！"
 fi
 
 # extract <zip> <file> <target dir> <junk paths>
-extract() {
+extract(){
   zip=$1
   file=$2
   dir=$3
   junk_paths=$4
-  [ -z "$junk_paths" ] && junk_paths=false
+  [ -z "${junk_paths}" ] && junk_paths=false
   opts="-o"
-  [ $junk_paths = true ] && opts="-oj"
+  [ ${junk_paths} = true ] && opts="-oj"
 
   file_path=""
   hash_path=""
-  if [ $junk_paths = true ]; then
-    file_path="$dir/$(basename "$file")"
-    hash_path="${TMPDIR}/$(basename "$file").sha256sum"
+  if [ ${junk_paths} = true ]; then
+    file_path="${dir}/$(basename "${file}")"
+    hash_path="${TMPDIR}/$(basename "${file}").sha256sum"
   else
-    file_path="$dir/$file"
-    hash_path="${TMPDIR}/$file.sha256sum"
+    file_path="${dir}/${file}"
+    hash_path="${TMPDIR}/${file}.sha256sum"
   fi
 
-  unzip $opts "$zip" "$file" -d "$dir" >&2
-  [ -f "$file_path" ] || abort "! $file 不存在！"
+  unzip ${opts} "${zip}" "${file}" -d "${dir}" >&2
+  [ -f "${file_path}" ] || abort "! ${file} 不存在！"
 
-  unzip $opts "$zip" "$file.sha256sum" -d "${TMPDIR}" >&2
-  [ -f "$hash_path" ] || abort "! $file.sha256sum 不存在！"
+  unzip ${opts} "${zip}" "${file}.sha256sum" -d "${TMPDIR}" >&2
+  [ -f "${hash_path}" ] || abort "! ${file}.sha256sum 不存在！"
 
-  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort "! $file 校验错误！"
-  Cus_Print "- 校验：$file" >&1
+  (echo "$(cat "${hash_path}")  ${file_path}" | sha256sum -c -s -) || abort "! 校验：${file} 错误！"
+  Cus_Print "- 校验：${file}" >&1
 }
 
-author="`grep_prop author $TMPDIR/module.prop`"
-name="`grep_prop name $TMPDIR/module.prop`"
+author="`grep_prop author ${TMPDIR}/module.prop`"
+name="`grep_prop name ${TMPDIR}/module.prop`"
 
 mkdir -p "${MODPATH}/files/bin"
 
-get_choose()
-{
+Get_Choose(){
 	local choose
 	local branch
 	while :; do
@@ -77,14 +75,13 @@ get_choose()
 				continue
 			;;
 		esac
-		echo "$branch"
+		echo "${branch}"
 		break
 	done
 }
 
 #Check whether the directory is readable and writable
-Sdcard_RW()
-{
+Sdcard_RW(){
 	local test_file="${DATADIR}/.A_TEST_FILE"
 	touch $test_file
 	rm $test_file
@@ -92,21 +89,21 @@ Sdcard_RW()
 
 #Check architecture
 Check_ARCH(){
-	case $ARCH in
+	case ${ARCH} in
 	arm64)
-		F_ARCH=$ARCH
+		F_ARCH=${ARCH}
 	;;
 	arm)
-		F_ARCH=$ARCH
+		F_ARCH=${ARCH}
 	;;
 	x64)
 		F_ARCH=x86_64
 	;;
 	x86)
-		F_ARCH=$ARCH
+		F_ARCH=${ARCH}
 	;;
 	*)
-		Cus_Print "- 不支持的架构: $ARCH"
+		Cus_Print "- 不支持的架构: ${ARCH}"
 		Cus_Print " "
 		abort "! 安装结束！"
 	;;
@@ -139,21 +136,21 @@ Cus_Print "- 运存大小: `free -m | grep -E "^Mem|^内存" | awk '{printf("总
 Cus_Print "- Swap大小: `free -m | grep -E "^Swap|^交换" | awk '{printf("总量：%s MB，已用：%s MB，剩余：%s MB，使用率：%.2f%%",$2,$3,$4,($3/$2*100))}'`"
 Cus_Print " "
 Cus_Print "(@) 模块信息："
-Cus_Print "- 名称: $name"
-Cus_Print "- 作者：$author"
+Cus_Print "- 名称: ${name}"
+Cus_Print "- 作者：${author}"
 Cus_Print " "
 Sdcard_RW
 [[ $? -ne 0 ]] && abort "! ${DATADIR} 目录读写测试失败，安装结束！"
 Check_ARCH
-Cus_Print "- 设备架构：$ARCH"
+Cus_Print "- 设备架构：${F_ARCH}"
 Cus_Print " "
 Cus_Print "(?) 确认安装吗？(请选择)"
 Cus_Print "- 按音量键＋: 安装 √"
 Cus_Print "- 按音量键－: 退出 ×"
-if [[ $(get_choose) -eq 0 ]]; then
+if [[ $(Get_Choose) -eq 0 ]]; then
 	Cus_Print "- 已选择安装！"
 	Cus_Print " "
-	Cus_Print "- 正在释放、校验文件！"
+	Cus_Print "- 正在校验、释放所需文件！"
 	extract "${ZIPFILE}" "files/bin/frpc-${F_ARCH}" "${MODPATH}/files/bin" true
 	extract "${ZIPFILE}" "files/bin/busybox_${F_ARCH}" "${MODPATH}/files/bin" true
 	extract "${ZIPFILE}" "service.sh" "${MODPATH}"
@@ -166,8 +163,8 @@ if [[ $(get_choose) -eq 0 ]]; then
 	extract "${ZIPFILE}" "files/frpc.ini" "${MODPATH}/files" true
 	extract "${ZIPFILE}" "files/frpc_full.ini" "${MODPATH}/files" true
 	Cus_Print "- 文件释放完成！正在设置权限！"
-	set_perm_recursive $MODPATH 0 0 0755 0644
-	set_perm_recursive  $MODPATH/files/bin 0 0 0755 0755
+	set_perm_recursive ${MODPATH} 0 0 0755 0644
+	set_perm_recursive  ${MODPATH}/files/bin 0 0 0755 0755
 	Cus_Print "- 权限设置完成！"
 	Cus_Print " "
 	Check_Crond
@@ -178,7 +175,7 @@ if [[ $(get_choose) -eq 0 ]]; then
 	Cus_Print "(?) 是否息屏检测配置文件状态？(请选择)"
 	Cus_Print "- 按音量键＋: 检  测 √"
 	Cus_Print "- 按音量键－: 不检测 ×"
-	if [[ $(get_choose) -eq 0 ]]; then
+	if [[ $(Get_Choose) -eq 0 ]]; then
 		sed -i -e "/^SCREEN_STATUS=/c SCREEN_STATUS=yes" "${MODPATH}/files/status.conf"
 		Cus_Print "- 设备息屏将检测配置文件状态！"
 		Cus_Print " "
@@ -187,17 +184,17 @@ if [[ $(get_choose) -eq 0 ]]; then
 		Cus_Print "- 设备息屏将不检测配置文件状态！"
 		Cus_Print " "
 	fi
-	if [[ -f $DATADIR/frpc/frpc.ini ]]; then
-		cp -af $MODPATH/update_log.md $DATADIR/frpc/
+	if [[ -f ${DATADIR}/frpc/frpc.ini ]]; then
+		cp -af ${MODPATH}/update_log.md ${DATADIR}/frpc/
 		Cus_Print "(?) 存在旧配置文件，是否保留原配置文件？(请选择)"
 		Cus_Print "- 按音量键＋: 保留"
 		Cus_Print "- 按音量键－: 替换"
-		if [[ $(get_choose) -eq 1 ]]; then
+		if [[ $(Get_Choose) -eq 1 ]]; then
 			Cus_Print "- 已选择替换备份原配置文件！"
 			now_date=$(date "+%Y%m%d%H%M%S")
-			mv $DATADIR/frpc/frpc.ini $DATADIR/frpc/backup_${now_date}-frpc.ini
+			mv ${DATADIR}/frpc/frpc.ini ${DATADIR}/frpc/backup_${now_date}-frpc.ini
 			Cus_Print "- 已备份保存为 Android/frpc/backup_${now_date}-frpc.ini"
-			cp -af $MODPATH/files/frpc.ini $DATADIR/frpc/
+			cp -af ${MODPATH}/files/frpc.ini ${DATADIR}/frpc/
 			Cus_Print "- 已创建新文件！"
 			Cus_Print " "
 		else
@@ -205,16 +202,16 @@ if [[ $(get_choose) -eq 0 ]]; then
 			Cus_Print " "
 		fi
 	else
-		if [[ ! -d $DATADIR/frpc ]]; then
-			mkdir -p $DATADIR/frpc
+		if [[ ! -d ${DATADIR}/frpc ]]; then
+			mkdir -p ${DATADIR}/frpc
 			Cus_Print "- 创建配置文件目录frpc完成！"
 			Cus_Print " "
-		elif [[ ! -d $DATADIR/frpc/logs ]]; then
-			mkdir $DATADIR/frpc/logs
+		elif [[ ! -d ${DATADIR}/frpc/logs ]]; then
+			mkdir ${DATADIR}/frpc/logs
 			Cus_Print "- 创建日志目录frpc/logs完成！"
 			Cus_Print " "
 		fi
-		cp -af $MODPATH/files/frpc*.ini $MODPATH/update_log.md $DATADIR/frpc/
+		cp -af ${MODPATH}/files/frpc*.ini ${MODPATH}/update_log.md ${DATADIR}/frpc/
 		Cus_Print "- 已创建配置文件！"
 		Cus_Print "- 请前往 ${DATADIR}/frpc目录查看frpc.ini文件内使用说明并配置文件！"
 		Cus_Print "- 然后进行设备重启即可！"

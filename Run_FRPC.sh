1e4691fe6ae306fef076714742594e557a51585e
Yang2635 <mail@isisy.com>
2022-06-02 15:46:25 +0800

    pid文件名调整

--- Run_FRPC.sh
+++ Run_FRPC.sh
@@ -2,39 +2,38 @@
 MODDIR=${0%/*}
 . ${MODDIR}/files/status.conf
 
-Start_FRPC(){
-	nohup ${MODDIR}/files/bin/frpc-${F_ARCH} -c ${DATADIR}/frpc/frpc.ini &>/dev/null &
-	echo "$!" >${MODDIR}/files/FRPC_RUN.pid
+Start_FRPC() {
+  nohup ${MODDIR}/files/bin/frpc-${F_ARCH} -c ${DATADIR}/frpc/frpc.ini >/dev/null 2>&1 &
+  echo "$!" >${MODDIR}/files/frpc_run.pid
 }
 
-Verify_FRPC(){
-	${MODDIR}/files/bin/frpc-${F_ARCH} verify -c ${DATADIR}/frpc/frpc.ini
+Verify_FRPC() {
+  ${MODDIR}/files/bin/frpc-${F_ARCH} verify -c ${DATADIR}/frpc/frpc.ini
 }
 
-Reload_FRPC(){
-	${MODDIR}/files/bin/frpc-${F_ARCH} reload -c ${DATADIR}/frpc/frpc.ini
+Reload_FRPC() {
+  ${MODDIR}/files/bin/frpc-${F_ARCH} reload -c ${DATADIR}/frpc/frpc.ini
 }
 
-Work_Status(){
-	${MODDIR}/files/bin/frpc-${F_ARCH} status -c ${DATADIR}/frpc/frpc.ini | grep "running" | wc -l
+Work_Status() {
+  ${MODDIR}/files/bin/frpc-${F_ARCH} status -c ${DATADIR}/frpc/frpc.ini | grep "running" | wc -l
 }
 
-
-if [[ $# -ne 0 ]]; then
-	case "$1" in
-		start)
-			Start_FRPC
-		;;
-		reload)
-			Reload_FRPC
-		;;
-		verify)
-			Verify_FRPC
-		;;
-		status)
-			Work_Status
-		;;
-	esac
+if [ $# -ne 0 ]; then
+  case "$1" in
+  start)
+    Start_FRPC
+    ;;
+  reload)
+    Reload_FRPC
+    ;;
+  verify)
+    Verify_FRPC
+    ;;
+  status)
+    Work_Status
+    ;;
+  esac
 else
-	exit 1
+  exit 1
 fi

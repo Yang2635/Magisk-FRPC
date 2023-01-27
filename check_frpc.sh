dbd014b3407b23235ed15408889415ac5f70b314
Yang2635 <mail@isisy.com>
2023-01-27 16:10:14 +0800

    简单调整

--- check_frpc.sh
+++ check_frpc.sh
@@ -4,10 +4,10 @@ set -u
 #MODDIR=${0%/*}
 MODDIR="$(dirname $(readlink -f "$0"))"
 . ${MODDIR}/files/status.conf
-. ${MODDIR}/functions.sh
 F_ARCH="${F_ARCH:=arm64}"
 DATADIR="${DATADIR:=/sdcard/Android}"
-Busybox_file="${MODDIR}/files/bin/busybox_${F_ARCH}"
+. ${MODDIR}/functions.sh
+export PATH=${MODDIR}/files/bin/busybox:$PATH
 frpc_bin="frpc-${F_ARCH}"
 
 if [ -z "$(cat ${MODDIR}/module.prop)" ]; then
@@ -29,7 +29,7 @@ running_start() {
   _check_file_status="$(stat -c %Y ${DATADIR}/frpc/frpc.ini)"
   sed -i "/^FILE_STATUS=/c FILE_STATUS=${_check_file_status}" "${MODDIR}/files/status.conf"
 
-  sh ${MODDIR}/Run_FRPC.sh verify
+  sh ${MODDIR}/run_frpc.sh verify
   if [ "$?" -eq 0 ]; then
     sed -i "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确！" "${MODDIR}/files/status.conf"
   else
@@ -38,7 +38,7 @@ running_start() {
       return 9
   fi
 
-  sh ${MODDIR}/Run_FRPC.sh start
+  sh ${MODDIR}/run_frpc.sh start
   sleep 5
   if [ -n "$(get_frpc_running_pid ${frpc_bin})" ]; then
     sed -i -e "/^RUNNING_STATUS=/c RUNNING_STATUS=FRPC正在运行中！" \
@@ -50,7 +50,7 @@ running_start() {
 
   _frpc_admin_port=$(get_parameters admin_port "${DATADIR}/frpc/frpc.ini")
   if [ "${_frpc_admin_port}" -ge 1 ] && [ "${_frpc_admin_port}" -le 65535 ]; then
-    _running_num=$(sh ${MODDIR}/Run_FRPC.sh status)
+    _running_num=$(sh ${MODDIR}/run_frpc.sh status)
     sleep 1
     sed -i "/^RUNNING_NUM=/c RUNNING_NUM=${_running_num}" "${MODDIR}/files/status.conf"
   elif [ -z "${_frpc_admin_port}" ]; then
@@ -76,20 +76,20 @@ check_reload() {
 
   _check_new_file_status="$(stat -c %Y ${DATADIR}/frpc/frpc.ini)"
   if [ "${FILE_STATUS}" != "${_check_new_file_status}" ]; then
-    sh ${MODDIR}/Run_FRPC.sh verify
+    sh ${MODDIR}/run_frpc.sh verify
     if [ "$?" -ne 0 ]; then
       sed -i "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测错误，可能修改的参数需重启生效，请自查配置文件或重启frpc程序！（已保持当前运行状态）" "${MODDIR}/files/status.conf"
       return 14
     fi
 
-    sh ${MODDIR}/Run_FRPC.sh reload
+    sh ${MODDIR}/run_frpc.sh reload
     if [ "$?" -ne 0 ]; then
       sed -i "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确但重载失败，可能修改的参数需重启生效，请自查配置文件或重启frpc程序！（已保持当前运行状态）" "${MODDIR}/files/status.conf"
       return 15
     fi
 
     sleep 5
-    _running_num=$(sh ${MODDIR}/Run_FRPC.sh status)
+    _running_num=$(sh ${MODDIR}/run_frpc.sh status)
     _reload_num="$(($RELOAD_NUM + 1))"
     sed -i -e "/^RELOAD_NUM=/c RELOAD_NUM=${_reload_num}" -e "/^FILE_STATUS=/c FILE_STATUS=${_check_new_file_status}" \
       -e "/^CHECK_FILE_STATUS=/c CHECK_FILE_STATUS=配置文件检测正确！" \
@@ -98,7 +98,7 @@ check_reload() {
 }
 
 battery_electricity_check() {
-  if [ "$(battery_electricity)" -lt 20 ] && [ "$(battery_charge)" -eq 0 ]; then
+  if [ "$(battery_electricity)" -lt 20 ] && [ "$(battery_charge)" -eq 1 ]; then
     if [ -n "${_frpc_pid_num}" ]; then
       {
         kill -9 ${_frpc_pid_num}
@@ -121,7 +121,6 @@ battery_electricity_check() {
 main() {
   local _frpc_vmrss
   local _frpc_pid_num="$(get_frpc_running_pid ${frpc_bin})"
-  #local _device_network_iface_number="$(device_network_iface_number)"
 
   if [ ! -f "${MODDIR}/disable" ]; then
     if [ -z "${_frpc_pid_num}" ]; then
@@ -145,9 +144,9 @@ main() {
   sleep 1
   . ${MODDIR}/files/status.conf
   if [ ! -f ${MODDIR}/update ]; then
-    sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：${RUNNING_STATUS}；物理内存占用：${_frpc_vmrss:-NuLL}]，[配置文件状态：${CHECK_FILE_STATUS}；穿透服务数（仅参考）：${RUNNING_NUM}，自动重载配置文件 ${RELOAD_NUM} 次]" "${MODDIR}/module.prop"
+    sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：${RUNNING_STATUS}；物理内存占用：${_frpc_vmrss:-NuLL}；配置文件状态：${CHECK_FILE_STATUS}；穿透服务数（仅参考）：${RUNNING_NUM}，自动重载配置文件 ${RELOAD_NUM} 次]" "${MODDIR}/module.prop"
   else
-    sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：${RUNNING_STATUS}，物理内存占用：${_frpc_vmrss:-NuLL}]，[配置文件状态：${CHECK_FILE_STATUS}；穿透服务数（仅参考）：${RUNNING_NUM}，自动重载配置文件 ${RELOAD_NUM} 次]（模块新设定将在设备重启后生效！）" "${MODDIR}/module.prop"
+    sed -i "/^description=/c description=使用Magisk挂载运行通用FRPC程序。[状态：${RUNNING_STATUS}，物理内存占用：${_frpc_vmrss:-NuLL}；配置文件状态：${CHECK_FILE_STATUS}；穿透服务数（仅参考）：${RUNNING_NUM}，自动重载配置文件 ${RELOAD_NUM} 次]（模块新设定将在设备重启后生效！）" "${MODDIR}/module.prop"
   fi
 }
 
